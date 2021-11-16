5b221786e25c924f290fa142c51706f60ad41b51
Ryan Jasinski <rjasinski@indeed.com>
2021-11-16 15:09:57 -0800

    option component and nesting component working

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -23,13 +36,18 @@ class Options extends React.Component {
   render() {
     return (
       <ol>
-        <li>One</li>
-        <li>Two</li>
+        <Option />
+        <Option />
       </ol>
     );
   }
 }
 
+class Option extends React.Component {
+  render() {
+    return <li>Option component here</li>;
+  }
+}
 class AddOption extends React.Component {
   render() {
     return (
@@ -39,12 +57,5 @@ class AddOption extends React.Component {
     );
   }
 }
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
-ReactDOM.render(jsx, document.getElementById("app"));
+
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
